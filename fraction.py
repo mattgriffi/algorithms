b739e47c09604859161c89c62ec37193be756084
mseffner <mseffner@gmail.com>
2017-05-25 15:10:41 -0700

    added __str__ method

--- fraction.py
+++ fraction.py
@@ -5,3 +5,7 @@ class Fraction:
     def __init__(self, numerator, denominator):
         self.num = numerator
         self.den = denominator
+
+
+    def __str__(self):
+        return "{}/{}".format(self.num, self.den)
