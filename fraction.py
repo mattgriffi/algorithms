5d8450cbaa41861300707bb71bd3b3f7ab5cd1e9
mseffner <mseffner@gmail.com>
2017-05-25 15:32:12 -0700

    added __add__ and __float__ methods

--- fraction.py
+++ fraction.py
@@ -9,3 +9,11 @@ class Fraction:
 
     def __str__(self):
         return "{}/{}".format(self.num, self.den)
+
+    def __add__(self, other):
+        new_num = self.num * other.den + self.den * other.num
+        new_den = self.den * other.den
+        return Fraction(new_num, new_den)
+
+    def __float__(self):
+        return self.num / self.den
