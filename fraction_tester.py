554a4cf5a71345b671f03239ef7d2880cef50946
mseffner <mseffner@gmail.com>
2017-05-25 15:22:42 -0700

    refactored unit test, added setUp

--- fraction_tester.py
+++ fraction_tester.py
@@ -6,14 +6,19 @@ from fraction import Fraction
 
 class TestFractionMethods(unittest.TestCase):
 
+    def setUp(self):
+        self.nums = [random.randint(0, 999) for _ in range(1000)]
+        self.denoms = [random.randint(0, 999) for _ in range(1000)]
+        self.pairs = zip(self.nums, self.denoms)
+
+
     def test_str(self):
         """Test the __str__ method"""
-        for _ in range(1000):
-            num = random.randint(0, 999)
-            denom = random.randint(0, 999)
-            frac = Fraction(num, denom)
-            correct = str(num) + "/" + str(denom)
-            self.assertEqual(str(frac), correct,
-                             "Fraction {} does not equal {}".format(
-                                 str(frac), correct
-                             ))
+        for n, d in self.pairs:
+            frac = Fraction(n, d)
+            correct = str(n) + "/" + str(d)
+            self.assertEqual(
+                    str(frac), correct,
+                    "Fraction {} does not equal {}".format(
+                        str(frac), correct
+                     ))
