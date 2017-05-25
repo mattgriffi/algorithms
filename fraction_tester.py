ab97698bd661f3b701ffbeb33e6263dad46bc698
mseffner <mseffner@gmail.com>
2017-05-25 15:41:13 -0700

    added unit tests for __float__ and __add__

--- fraction_tester.py
+++ fraction_tester.py
@@ -8,7 +8,7 @@ class TestFractionMethods(unittest.TestCase):
 
     def setUp(self):
         self.nums = [random.randint(0, 999) for _ in range(1000)]
-        self.denoms = [random.randint(0, 999) for _ in range(1000)]
+        self.denoms = [random.randint(1, 999) for _ in range(1000)]
         self.pairs = zip(self.nums, self.denoms)
 
 
@@ -17,8 +17,23 @@ class TestFractionMethods(unittest.TestCase):
         for n, d in self.pairs:
             frac = Fraction(n, d)
             correct = str(n) + "/" + str(d)
-            self.assertEqual(
-                    str(frac), correct,
-                    "Fraction {} does not equal {}".format(
-                        str(frac), correct
-                     ))
+            self.assertEqual(str(frac), correct)
+
+
+    def test_float(self):
+        for n, d in self.pairs:
+            frac = Fraction(n, d)
+            frac_float = float(frac)
+            correct = n / d
+            self.assertAlmostEqual(frac_float, correct, places=10)
+
+
+    def test_add(self):
+        for n, d in self.pairs:
+            num_add = random.randint(0, 999)
+            den_add = random.randint(1, 999)
+            correct = n / d + num_add / den_add
+            frac1 = Fraction(n, d)
+            frac2 = Fraction(num_add, den_add)
+            result = frac1 + frac2
+            self.assertAlmostEqual(float(result), correct, places=10)
