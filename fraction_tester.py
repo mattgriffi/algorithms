1ac86a0cce8f7b4bfdb3b586d67e5b1cf7f24e82
mseffner <mseffner@gmail.com>
2017-05-25 15:11:08 -0700

    added unit test for __str__ method

--- fraction_tester.py
+++ fraction_tester.py
@@ -1,8 +1,19 @@
 # This class tests all method of the Fraction class
 
 import unittest
+import random
 from fraction import Fraction
 
 class TestFractionMethods(unittest.TestCase):
 
-    pass
+    def test_str(self):
+        """Test the __str__ method"""
+        for _ in range(1000):
+            num = random.randint(0, 999)
+            denom = random.randint(0, 999)
+            frac = Fraction(num, denom)
+            correct = str(num) + "/" + str(denom)
+            self.assertEqual(str(frac), correct,
+                             "Fraction {} does not equal {}".format(
+                                 str(frac), correct
+                             ))
