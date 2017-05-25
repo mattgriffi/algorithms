9e851995a86518373521b5318799894a296e9ad5
mseffner <mseffner@gmail.com>
2017-05-25 16:18:15 -0700

    added methods for __eq__ and reduction, unit test for __eq__

--- fraction.py
+++ fraction.py
@@ -22,3 +22,22 @@ class Fraction:
         new_num = self.num * other.den + self.den * other.num
         new_den = self.den * other.den
         return Fraction(new_num, new_den)
+
+    def __eq__(self, other):
+        return type(other) == type(self) and (
+            self.num == other.num == 0 or
+            (self.num == other.num and self.den == other.den)
+        )
+
+    def reduce(self):
+        """Reduces fraction to lowest terms"""
+        gcd = self.gcd(self.num, self.den)
+        self.num = self.num / gcd
+        self.den = self.den / gcd
+
+    def gcd(self, m, n):
+        """Using Euclid's Algorithm to find the GCD of m and n"""
+        while m % n:
+            old_m, old_n = m, n
+            m, n = old_n, old_m % old_n
+        return n
--- fraction_tester.py
+++ fraction_tester.py
@@ -2,7 +2,7 @@
 
 import unittest
 import random
-from fraction import Fraction
+from fraction import Fraction as F
 
 class TestFractionMethods(unittest.TestCase):
 
@@ -13,13 +13,13 @@ class TestFractionMethods(unittest.TestCase):
 
     def test_str(self):
         for n, d in self.pairs:
-            frac = Fraction(n, d)
+            frac = F(n, d)
             correct = str(n) + "/" + str(d)
             self.assertEqual(str(frac), correct)
 
     def test_float(self):
         for n, d in self.pairs:
-            frac = Fraction(n, d)
+            frac = F(n, d)
             frac_float = float(frac)
             correct = n / d
             self.assertAlmostEqual(frac_float, correct, places=10)
@@ -29,22 +29,37 @@ class TestFractionMethods(unittest.TestCase):
             num_add = random.randint(0, 999)
             den_add = random.randint(1, 999)
             correct = n / d + num_add / den_add
-            frac1 = Fraction(n, d)
-            frac2 = Fraction(num_add, den_add)
+            frac1 = F(n, d)
+            frac2 = F(num_add, den_add)
             result = frac1 + frac2
             self.assertAlmostEqual(float(result), correct, places=10)
 
     def test_int(self):
         for n, d in self.pairs:
-            frac = Fraction(n, d)
+            frac = F(n, d)
             frac_int = int(frac)
             correct = n // d
             self.assertEqual(frac_int, correct)
 
     def test_bool(self):
         for n, d in self.pairs:
-            frac = Fraction(n, d)
+            frac = F(n, d)
             self.assertTrue(bool(n) == bool(frac))
+
+    def test_eq(self):
+        for n, d in self.pairs:
+            frac1 = F(n, d)
+            frac2 = F(n, d)
+            self.assertTrue(frac1 == frac2)
+        frac1 = F(3, 5)
+        frac2 = F(2, 9)
+        self.assertFalse(frac1 == 3)
+        self.assertFalse(frac1 == range(3))
+        self.assertFalse(frac1 == frac2)
+        self.assertTrue(F(0, 5) == F(0, 3))
+
+    def test_reduce(self):
+        pass
 
 if __name__ == "__main__":
     unittest.main()
