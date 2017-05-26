34c2c34c5a0c84187330822c6e30968166c67ea5
mseffner <mseffner@gmail.com>
2017-05-26 09:22:45 -0700

    n^2 anagram solver

--- anagram_detecter.py
+++ anagram_detecter.py
@@ -1,5 +1,28 @@
 """This program detects whether the given string is an anagram of another given string"""
 
 
+def main():
+    print(anagram_solver("abc", "cba"))
+    print(anagram_solver("level", "velel"))
+    print(anagram_solver("dog", "cat"))
+    print(anagram_solver("god", "dog"))
+
+
 def anagram_solver(a, b):
-    pass
+    c = list(b)
+    # Iterate over string a
+    for ca in a:
+        found = False
+        # Iterate over string b, comparing char of a to chars in b
+        for i, cb in enumerate(c):
+            if ca == cb:
+                c[i] = None
+                found = True
+                break
+        if not found:
+            return False
+    return True
+
+
+if __name__ == "__main__":
+    main()
