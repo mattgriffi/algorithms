ab66e974bf79b9894022871d6713b26f9686c51d
mseffner <mseffner@gmail.com>
2017-05-26 09:45:34 -0700

    added improved solver

--- anagram_detecter.py
+++ anagram_detecter.py
@@ -7,10 +7,13 @@ import time
 def main():
     a = "klahsfdkljhasfpiouweafoihdsfa;jskl;sfjapoweiuaypofsafhsjkfhskjfnasfhjpwiureasf"*50
     b = "".join(sorted(a))
-    result_format = "Correct answer: {}\nDerived answer: {}\nTime taken: {:.15f}\n"
-    print(result_format.format(True, *anagram_solver("level", "velel")))
-    print(result_format.format(False, *anagram_solver("dog", "cat")))
-    print(result_format.format(True, *anagram_solver(a, b)))
+    c = b[:-1] + "5"
+    result_format = "Algorithm: {}\nCorrect answer: {}\nDerived answer: {}\nTime taken: {:.15f}\n"
+    print(result_format.format("Slow", True, *anagram_solver(a, b)))
+    print(result_format.format("Slow", False, *anagram_solver(a, c)))
+
+    print(result_format.format("Fast", True, *better_solver(a, b)))
+    print(result_format.format("Fast", False, *better_solver(a, c)))
 
 
 def anagram_solver(a, b):
@@ -31,7 +34,13 @@ def anagram_solver(a, b):
 
 
 def better_solver(a, b):
-    pass
+    start = time.time()
+    if set(a) != set(b):
+        return False, time.time() - start
+    for c in a:
+        if a.count(c) != b.count(c):
+            return False, time.time() - start
+    return True, time.time() - start
 
 
 if __name__ == "__main__":
