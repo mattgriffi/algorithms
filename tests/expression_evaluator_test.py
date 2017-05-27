fd677f5648a51ab79002ee03f4ebb6de14fac85a
mseffner <mseffner@gmail.com>
2017-05-27 15:04:20 -0700

    unit test stub

---
+++ tests/expression_evaluator_test.py
@@ -0,0 +1,17 @@
+"""Unit tests for the ExpressionEvaluator"""
+
+
+import unittest
+
+from expression_evaluator import ExpressionEvaluator as EE
+
+
+class ExpressionEvaluatorTester(unittest.TestCase):
+
+    def setUp(self):
+        a = "10 + 3 * 5 / ( 16 - 4 )"
+        aa = 11.25
+
+
+if __name__ == "__main__":
+    unittest.main()
