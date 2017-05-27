f84d7150d6e2cb8f3b11026027093a8399d0646a
mseffner <mseffner@gmail.com>
2017-05-27 14:14:21 -0700

    infix_to_postfix extended to support parentheses, added comments

--- expression_evaluator.py
+++ expression_evaluator.py
@@ -14,7 +14,7 @@ from operators import Operators
 
 
 def main():
-    exp = "A * B + C * D"
+    exp = "( A + B + C ) * D"
     print(ExpressionEvaluator.infix_to_postfix(exp))
 
 
@@ -25,23 +25,42 @@ class ExpressionEvaluator:
 
     @staticmethod
     def infix_to_postfix(exp):
-        precedence = {'+': 1, '-': 1, '*': 2, '/': 2}
+        """Converts an expression in infix order to an equivalent
+        expression in postfix order.
+        Example:
+        A * B + C * D  ->  A B * C D * +
+        """
+        precedence = {'(': 0, '+': 1, '-': 1, '*': 2, '/': 2}
         exp = exp.split()
         result = []
         opstack = Stack()
 
-        for c in exp:
-            if c not in precedence:
-                result.append(c)
+        for e in exp:
+            # If e is a closing parenthesis, pop the stack until we reach
+            # the opening parenthesis and append all those operators to the result
+            if e == ')':
+                while opstack.peek() != '(':
+                    result.append(opstack.pop())
+                opstack.pop()
+            # If e is not an operator, append to result
+            elif e not in precedence:
+                result.append(e)
+            # If there's nothing already on the stack, push e
             elif opstack.isEmpty():
-                opstack.push(c)
-            elif precedence[opstack.peek()] >= precedence[c]:
+                opstack.push(e)
+            # If the top operator on the stack is higher precedence than e,
+            # append that operator to the result and push e.
+            # Ignore opening parens
+            elif precedence[opstack.peek()] >= precedence[e] and e != '(':
                 result.append(opstack.pop())
-                opstack.push(c)
+                opstack.push(e)
+            # Else, push e to stack
             else:
-                opstack.push(c)
+                opstack.push(e)
+        # Append every operator left on the stack
         while not opstack.isEmpty():
             result.append(opstack.pop())
+
         return ' '.join(result)
 
 
