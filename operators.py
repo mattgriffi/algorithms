772ed3ab620cbec119156a72ab819c4f063d3736
mseffner <mseffner@gmail.com>
2017-05-27 13:27:16 -0700

    created class for arithmetic operations

---
+++ operators.py
@@ -0,0 +1,20 @@
+"""This class defines some static methods for basic arithmetic operations."""
+
+
+class Operators:
+
+    @staticmethod
+    def add(a, b):
+        return a + b
+
+    @staticmethod
+    def sub(a, b):
+        return a - b
+
+    @staticmethod
+    def mul(a, b):
+        return a * b
+
+    @staticmethod
+    def div(a, b):
+        return a // b
