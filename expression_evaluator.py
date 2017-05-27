e1e09ae5b25020479cd26913d8273abfc0fb66e8
mseffner <mseffner@gmail.com>
2017-05-27 13:49:20 -0700

    infix_to_postfix method working

--- expression_evaluator.py
+++ expression_evaluator.py
@@ -10,5 +10,40 @@ Examples:
 
 
 from data_structures.stack import Stack
+from operators import Operators
 
 
+def main():
+    exp = "A * B + C * D"
+    print(ExpressionEvaluator.infix_to_postfix(exp))
+
+
+class ExpressionEvaluator:
+
+    operators = {'+': Operators.add, '-': Operators.sub,
+                 '*': Operators.mul, '/': Operators.div}
+
+    @staticmethod
+    def infix_to_postfix(exp):
+        precedence = {'+': 1, '-': 1, '*': 2, '/': 2}
+        exp = exp.split()
+        result = []
+        opstack = Stack()
+
+        for c in exp:
+            if c not in precedence:
+                result.append(c)
+            elif opstack.isEmpty():
+                opstack.push(c)
+            elif precedence[opstack.peek()] >= precedence[c]:
+                result.append(opstack.pop())
+                opstack.push(c)
+            else:
+                opstack.push(c)
+        while not opstack.isEmpty():
+            result.append(opstack.pop())
+        return ' '.join(result)
+
+
+if __name__ == "__main__":
+    main()
