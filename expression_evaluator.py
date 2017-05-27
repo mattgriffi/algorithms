2db2461dd8de460e715162568d43cf685b316138
mseffner <mseffner@gmail.com>
2017-05-27 12:54:12 -0700

    expression evaluator stub

---
+++ expression_evaluator.py
@@ -0,0 +1,11 @@
+"""This program evaluates a given mathematical expression.
+The expression should be in either normal, infix order, or
+in postfix order. All operands, operators, and parenthesis
+should be space-separated.
+
+Examples:
+    Infix: ( A + B ) * C
+    Postfix: A B + C *
+"""
+
+
