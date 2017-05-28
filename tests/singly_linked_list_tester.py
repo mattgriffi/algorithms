39f0baa9bdbec77111f7f13be5cd005b1d1a9005
mseffner <mseffner@gmail.com>
2017-05-28 08:38:40 -0700

    added unit test stubs

---
+++ tests/singly_linked_list_tester.py
@@ -0,0 +1,25 @@
+"""Unit tests for SinglyLinkedList."""
+
+import random
+import unittest
+
+from data_structures.singly_linked_list import SinglyLinkedList, ListException
+
+class SinglyLinkedListTester(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_add_remove(self):
+        pass
+
+    def test_pop(self):
+        pass
+
+    def test_search_index(self):
+        pass
+
+    def test_all(self):
+        pass
+
+    def test_fuzz(self):
+        pass
