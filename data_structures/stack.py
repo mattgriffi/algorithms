af280e2fb1b6a498794aae42cbcd73fbcd430805
mseffner <mseffner@gmail.com>
2017-05-27 19:14:44 -0700

    changed stack to use built-in len() instead of tracking size with an instance variable

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -8,35 +8,32 @@ class Stack:
 
     def __init__(self):
         self.stack = []
-        self.s_size = 0
 
     def push(self, item):
         """Pushes item onto stack."""
-        self.s_size += 1
         self.stack.append(item)
 
     def pop(self):
         """Removes and return the top element of the stack.
         Raises a StackException if the stack is empty."""
-        if self.s_size == 0:
+        if not len(self.stack):
             raise StackException("cannot pop from an empty stack")
-        self.s_size -= 1
         return self.stack.pop()
 
     def peek(self):
         """Returns the top element of the stack, without removing it.
         Raises a StackException if the stack is empty."""
-        if self.s_size == 0:
+        if not len(self.stack):
             raise StackException("cannot peek at an empty stack")
         return self.stack[-1]
 
     def isEmpty(self) -> bool:
         """Returns True if the stack is empty, else False."""
-        return self.s_size == 0
+        return len(self.stack) == 0
 
     def size(self) -> int:
         """Returns the number of elements currently in the stack"""
-        return self.s_size
+        return len(self.stack)
 
 
 class StackException(Exception):
