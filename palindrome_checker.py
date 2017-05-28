55b7335287f84b226dc2148310b6c8cb981beac6
mseffner <mseffner@gmail.com>
2017-05-28 07:48:39 -0700

    added comments

--- palindrome_checker.py
+++ palindrome_checker.py
@@ -10,8 +10,11 @@ class PalindromeChecker:
     @staticmethod
     def check(s):
         deque = Deque()
+        # Put each character in the deque (addRear is faster than addFront)
         for c in s:
             deque.addRear(c)
+        # Match the front and rear chars in the deque. If we're left with 1 char, it was the
+        # middle char and in an odd-length string and can be ignored
         while deque.size() > 1:
             if deque.removeFront() != deque.removeRear():
                 return False
