8e8bc51d3dde017b11839c2713934e3b2a2985ce
mseffner <mseffner@gmail.com>
2017-05-27 21:05:02 -0700

    hot_potato stub

---
+++ hot_potato.py
@@ -0,0 +1,12 @@
+"""This program uses a Queue to simulate the children's game of hot potato."""
+
+
+from data_structures.queue import Queue
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
