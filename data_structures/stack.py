2b62269caa21a7aee9f2d71209b26da0c741e208
mseffner <mseffner@gmail.com>
2017-05-27 17:08:23 -0700

    fixed docstring and added other comments

--- data_structures/stack.py
+++ data_structures/stack.py
@@ -1,5 +1,4 @@
 """This class implements a LIFO Stack with the usual methods.
-Does not do any error handling.
 
 All operations in this stack are constant time.
 """
@@ -12,24 +11,31 @@ class Stack:
         self.s_size = 0
 
     def push(self, item):
+        """Pushes item onto stack."""
         self.s_size += 1
         self.stack.append(item)
 
     def pop(self):
+        """Removes and return the top element of the stack.
+        Raises a StackException if the stack is empty."""
         if self.s_size == 0:
             raise StackException("cannot pop from an empty stack")
         self.s_size -= 1
         return self.stack.pop()
 
     def peek(self):
+        """Returns the top element of the stack, without removing it.
+        Raises a StackException if the stack is empty."""
         if self.s_size == 0:
             raise StackException("cannot peek at an empty stack")
         return self.stack[-1]
 
-    def isEmpty(self):
+    def isEmpty(self) -> bool:
+        """Returns True if the stack is empty, else False."""
         return self.s_size == 0
 
-    def size(self):
+    def size(self) -> int:
+        """Returns the number of elements currently in the stack"""
         return self.s_size
 
 
