04350b01bee9767ee5e27ed1af72f42c9fbfc22f
mseffner <mseffner@gmail.com>
2017-05-28 08:24:28 -0700

    singly linked list stub

---
+++ data_structures/singly_linked_list.py
@@ -0,0 +1,11 @@
+"""This class implements a singly-linked list."""
+
+
+class SinglyLinkedList:
+    def __init__(self):
+        pass
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
