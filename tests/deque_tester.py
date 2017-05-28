e6bd6f57e624960e236ae9b86806e7e28ca23bde
mseffner <mseffner@gmail.com>
2017-05-28 06:53:50 -0700

    deque unit test stub

---
+++ tests/deque_tester.py
@@ -0,0 +1,24 @@
+"""Unit tests for deque."""
+
+
+import random
+import unittest
+
+from data_structures.deque import Deque, DequeException
+
+
+class DequeTester(unittest.TestCase):
+
+    def setUp(self):
+        self.d = Deque
+        self.sim = []
+
+    def test_deque(self):
+        pass
+
+    def test_fuzz(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
