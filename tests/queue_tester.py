27747129cfb20fc18620b3bf237bb08a5c8875e0
mseffner <mseffner@gmail.com>
2017-05-27 19:58:29 -0700

    added unit test stub for Queue

---
+++ tests/queue_tester.py
@@ -0,0 +1,15 @@
+"""Unit tests for Queue."""
+
+import unittest
+
+from data_structures.queue import Queue
+
+
+class QueueTester(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
