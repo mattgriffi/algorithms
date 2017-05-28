e9711e2ab2d640deeceb49dfce26a55b2bc94b9d
mseffner <mseffner@gmail.com>
2017-05-27 20:03:29 -0700

    added method stubs and comments

--- data_structures/queue.py
+++ data_structures/queue.py
@@ -7,3 +7,19 @@ class Queue:
         self.q = []
         self.qsize = 0
         self.front = 0
+
+    def enqueue(self, item):
+        """Adds item to the end of the queue."""
+        pass
+
+    def dequeue(self):
+        """Returns element at the front of the queue and removes it from the queue."""
+        pass
+
+    def isEmpty(self):
+        """Returns True if there is nothing in the queue, else False."""
+        pass
+
+    def size(self):
+        """Returns the number of elements currently in the queue."""
+        pass
