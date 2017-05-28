4c01566ba720d8b43051a275a120bad65ad69410
mseffner <mseffner@gmail.com>
2017-05-27 21:25:38 -0700

    docstring and stub

---
+++ printing_simulation.py
@@ -0,0 +1,17 @@
+"""This program is a simulation of the following problem:
+
+On average, there are 10 students in the computer lab in any given hour. There is 1 printer in
+this computer lab. On average, each student will need to use the printer twice, and will need
+to print between 1 and 20 pages. The printer has two settings: it can print lower quality
+at a rate of 10 pages per minute, or higher quality at a rate of 5 pages per minute.
+
+What is the average waiting time per print task for these two methods?
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
