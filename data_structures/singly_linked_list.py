ea8806585454909351fc323cb4f8ce4e9c2d658f
mseffner <mseffner@gmail.com>
2017-05-28 08:34:56 -0700

    added method stubs and comments

--- data_structures/singly_linked_list.py
+++ data_structures/singly_linked_list.py
@@ -3,7 +3,36 @@
 
 class SinglyLinkedList:
     def __init__(self):
-        pass
+        self.head = None
+
+    def add(self, item):
+        """Adds item to the start of the list."""
+
+    def remove(self, item):
+        """Removes given item from the list."""
+
+    def search(self, item):
+        """Returns True if item is in the list, else False."""
+
+    def isEmpty(self):
+        """Returns True if the list is empty, else False."""
+
+    def size(self):
+        """Returns the number of items currently in the list."""
+
+    def append(self, item):
+        """Adds item to the end of the list."""
+
+    def index(self, item):
+        """Returns the index of the first occurrence of item in the list."""
+
+    def insert(self, pos, item):
+        """Inserts item at pos in the list, moving other items back."""
+
+    def pop(self, pos=-1):
+        """Returns item at given pos, and removes it from the list. Will return and remove
+        the last item in the list by default."""
+
 
 class Node:
     def __init__(self, data):
