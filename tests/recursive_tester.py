5f2477298c708e3bd79991cdc33609b3be70d896
mseffner <mseffner@gmail.com>
2017-05-30 18:46:23 -0700

    unit test stub

---
+++ tests/recursive_tester.py
@@ -0,0 +1,20 @@
+"""Tester for various recursive methods."""
+
+
+import random
+import unittest
+
+from recursive_practice import factorial
+
+
+class RecursiveTester(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_factorial(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
