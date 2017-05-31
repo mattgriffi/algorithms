a136b34019a6eb2df620b691da43bd418eeda715
mseffner <mseffner@gmail.com>
2017-05-31 14:20:58 -0700

    added random tester for binary search

--- tests/search_tester.py
+++ tests/search_tester.py
@@ -50,6 +50,19 @@ class SearchTester(unittest.TestCase):
         self.assertEqual(0 in self.c, binary_search(self.c, 0))
         self.assertEqual(-1 in self.c, binary_search(self.c, -1))
 
+    def test_binary_search_fuzz(self):
+
+        for _ in range(20):
+
+            a = [random.randint(-100, 100) for _ in range(100)]
+            a.sort()
+
+            for e in a:
+                self.assertEqual(e in a, binary_search(a, e))
+            for _ in range(10000):
+                i = random.randint(-100, 100)
+                self.assertEqual(i in a, binary_search(a, i))
+
 
 if __name__ == "__main__":
     unittest.main()
