a823ec2d79918583a736d1ed76f9e0b0d79ff290
mseffner <mseffner@gmail.com>
2017-05-31 13:47:52 -0700

    implemented sequential search

--- search_algorithms.py
+++ search_algorithms.py
@@ -1,3 +1,14 @@
 """This program implements several numeric searching algorithms."""
 
 
+def sequential_search(a: list, x) -> bool:
+    """This function determines whether item x is in list a. It does so by checking every
+    element in the list sequentially. Also known as "linear search."
+    Time complexity of O(n)
+    """
+    i = 0
+    while i < len(a):
+        if a[i] == x:
+            return True
+        i += 1
+    return False
--- tests/search_tester.py
+++ tests/search_tester.py
@@ -4,14 +4,28 @@
 import random
 import unittest
 
+from search_algorithms import sequential_search
+
 
 class SearchTester(unittest.TestCase):
 
     def setUp(self):
-        pass
+        self.a = []
+        self.b = [1]
+        self.c = [1, 2, 3, 4]
+        self.d = [1, 2, True, "hello"]
 
     def test_sequential_search(self):
-        pass
+        self.assertEqual(1 in self.a, sequential_search(self.a, 1))
+        self.assertEqual(1 in self.b, sequential_search(self.b, 1))
+        self.assertEqual(2 in self.b, sequential_search(self.b, 2))
+        self.assertEqual(3 in self.c, sequential_search(self.c, 3))
+        self.assertEqual(5 in self.c, sequential_search(self.c, 5))
+        self.assertEqual(1 in self.d, sequential_search(self.d, 1))
+        self.assertEqual(True in self.d, sequential_search(self.d, True))
+        self.assertEqual("hello" in self.d, sequential_search(self.d, "hello"))
+        self.assertEqual("hi" in self.d, sequential_search(self.d, "hi"))
+        self.assertEqual(False in self.d, sequential_search(self.d, False))
 
     def test_binary_search(self):
         pass
