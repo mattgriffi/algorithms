e75ad07d2093565cf04472d196760ad696c945f2
mseffner <mseffner@gmail.com>
2017-05-30 18:50:09 -0700

    factorial tester

--- tests/recursive_tester.py
+++ tests/recursive_tester.py
@@ -13,7 +13,17 @@ class RecursiveTester(unittest.TestCase):
         pass
 
     def test_factorial(self):
-        pass
+        with self.assertRaises(Exception):
+            factorial(-1)
+        self.assertEqual(1, factorial(0))
+        self.assertEqual(1, factorial(1))
+        self.assertEqual(2, factorial(2))
+        self.assertEqual(6, factorial(3))
+        self.assertEqual(24, factorial(4))
+        self.assertEqual(120, factorial(5))
+        self.assertEqual(720, factorial(6))
+        self.assertEqual(5040, factorial(7))
+        self.assertEqual(265252859812191058636308480000000, factorial(30))
 
 
 if __name__ == "__main__":
