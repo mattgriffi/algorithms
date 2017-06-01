bd5c096736395a4d1ee4973b45b0dd43753af79c
mseffner <mseffner@gmail.com>
2017-05-31 17:40:52 -0700

    added constructor and Pair class

--- data_structures/hash_map.py
+++ data_structures/hash_map.py
@@ -1,10 +1,18 @@
 """This class implements a hash map."""
 
 
+from data_structures.singly_linked_list import SinglyLinkedList
+
+
 class Map:
 
-    def __init__(self):
-        pass
+    def __init__(self, size):
+        """Creates a map with specified size. Note that size does not limit the number of
+        key-value pairs that can be held in the map, but rather it controls the number of
+        addresses available for hashing and storing keys. A larger size will reduce the odds
+        of collisions, at the cost of initial memory usage."""
+        self.size = size
+        self.table = [None] * size
 
     def put(self, key, val):
         """Add a new key-value pair. If key already exists in map, its value will be
@@ -19,8 +27,14 @@ class Map:
     def __hash(self, key):
         """Returns the hash code for the given key."""
 
-    def __del__(self, key):
+    def __delitem__(self, key):
         pass
 
     def __contains__(self, key):
         pass
+
+
+class Pair:
+    def __init__(self, key, value):
+        self.key = key
+        self.value = value
