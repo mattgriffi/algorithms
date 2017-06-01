37f5fe0b2744eeb5bbbcc7668c9996e86c8e2d98
mseffner <mseffner@gmail.com>
2017-05-31 17:19:51 -0700

    added Map class with method stubs

---
+++ data_structures/hash_map.py
@@ -0,0 +1,26 @@
+"""This class implements a hash map."""
+
+
+class Map:
+
+    def __init__(self):
+        pass
+
+    def put(self, key, val):
+        """Add a new key-value pair. If key already exists in map, its value will be
+        overwritten."""
+
+    def get(self, key):
+        """Returns the value of give key, or None if key is not in the map."""
+
+    def len(self):
+        """Returns the number of key-value pairs in the map."""
+
+    def __hash(self, key):
+        """Returns the hash code for the given key."""
+
+    def __del__(self, key):
+        pass
+
+    def __contains__(self, key):
+        pass
