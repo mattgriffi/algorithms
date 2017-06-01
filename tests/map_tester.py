b9cb89131c4ec0551a5633e1a96bd5a9e3bde8c0
mseffner <mseffner@gmail.com>
2017-05-31 18:15:23 -0700

    unit test stub for Map

---
+++ tests/map_tester.py
@@ -0,0 +1,15 @@
+"""Unit tests for the Map class."""
+
+
+import random
+import unittest
+
+
+class MapTester(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
