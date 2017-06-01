fa47f7ee2d707202fb2bed791e26d6b62a5571be
mseffner <mseffner@gmail.com>
2017-06-01 09:51:08 -0700

    created function stubs

---
+++ sorting_algorithms.py
@@ -0,0 +1,25 @@
+"""This programs implements various sorting algorithms."""
+
+
+def bubble_sort(a: list):
+    pass
+
+
+def selection_sort(a: list):
+    pass
+
+
+def insertion_sort(a: list):
+    pass
+
+
+def shell_sort(a: list):
+    pass
+
+
+def merge_sort(a: list):
+    pass
+
+
+def quick_sort(a: list):
+    pass
