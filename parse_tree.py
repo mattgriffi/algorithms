e42be77fa68eb1b299e20f47775c9521b7ba8168
mseffner <mseffner@gmail.com>
2017-06-02 16:02:31 -0700

    added comments

--- parse_tree.py
+++ parse_tree.py
@@ -6,26 +6,35 @@ from data_structures.binary_tree import BinaryTree
 
 
 def build_parse_tree(exp):
+
     tokens = exp.split()
     operators = {"+", "-", "*", "/", "//", "%", "**"}
     tree = BinaryTree()
     node_stack = Stack()
     current_node = tree
 
+    node_stack.push(current_node)
+
     for token in tokens:
+        # If "(", make a new node to the left and move to it
         if token == "(":
             current_node.insert_left()
             node_stack.push(current_node)
             current_node = current_node.left
+        # If ")", move back to the parent node
         elif token == ")":
             current_node = node_stack.pop()
+        # If operator, put operator in current node, make new node to the right, and move to it
         elif token in operators:
             current_node.key = token
             current_node.insert_right()
             node_stack.push(current_node)
             current_node = current_node.right
+        # Otherwise it must be an integer, so convert to int, put in node, then move to parent
         else:
             current_node.key = int(token)
             current_node = node_stack.pop()
 
     return tree
+
+build_parse_tree("( ( 10 + 5 ) * 3 )")
