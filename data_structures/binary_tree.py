8f20c9584e830170e98df3433dbb07ad1aaa8753
mseffner <mseffner@gmail.com>
2017-06-02 15:12:21 -0700

    implemented basic recursive binary tree

---
+++ data_structures/binary_tree.py
@@ -0,0 +1,25 @@
+"""This class implements a recursive binary tree."""
+
+
+class BinaryTree:
+
+    def __init__(self, root):
+        self.key = root
+        self.left = None
+        self.right = None
+
+    def insert_left(self, key):
+        if self.left is None:
+            self.left = BinaryTree(key)
+        else:
+            new = BinaryTree(key)
+            new.left = self.left
+            self.left = new
+
+    def insert_right(self, key):
+        if self.right is None:
+            self.right = BinaryTree(key)
+        else:
+            new = BinaryTree(key)
+            new.right = self.right
+            self.right = new
