2c3fc573ef9fffd6457e903a1e5ac9b99be9fa06
mseffner <mseffner@gmail.com>
2017-06-02 21:09:05 -0700

    added test for insert method

--- tests/heap_tester.py
+++ tests/heap_tester.py
@@ -1,6 +1,7 @@
 """Unit tests for binary heaps."""
 
 
+import random
 import unittest
 
 from data_structures.min_heap import MinHeap
@@ -8,7 +9,15 @@ from data_structures.min_heap import MinHeap
 
 class HeapTester(unittest.TestCase):
     def setUp(self):
-        pass
+        self.h = MinHeap()
+
+    def test_insert(self):
+        for _ in range(1000):
+            self.h.insert(random.randint(-100, 100))
+            self.check_min(self.h)
+
+    def check_min(self, heap):
+        self.assertEqual(min(heap.heap[1:]), heap.heap[1], f"\n{heap.heap}")
 
 
 if __name__ == "__main__":
