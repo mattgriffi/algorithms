0600f5221024e0e160b2700e3ac2a2012582117c
mseffner <mseffner@gmail.com>
2017-06-02 22:07:23 -0700

    implemented fuzzer

--- tests/heap_tester.py
+++ tests/heap_tester.py
@@ -4,10 +4,11 @@
 import random
 import unittest
 
-from data_structures.min_heap import MinHeap
+from data_structures.min_heap import MinHeap, HeapException
 
 
 class HeapTester(unittest.TestCase):
+
     def setUp(self):
         self.h = MinHeap()
 
@@ -27,6 +28,47 @@ class HeapTester(unittest.TestCase):
             self.check_min(self.h)
             sorted_list.append(min_item)
         self.assertTrue(sorted(sorted_list) == sorted_list)
+
+    def test_heap_fuzzer(self):
+
+        for _ in range(20):
+
+            h = MinHeap()
+            size = 0
+
+            # Make sure it behaves properly after instantiation
+            self.assertTrue(h.is_empty())
+            with self.assertRaises(HeapException):
+                h.find_min()
+            with self.assertRaises(HeapException):
+                h.del_min()
+            self.assertEqual(size, h.size())
+
+            # Set the bias toward insertion operations
+            bias = random.uniform(0.45, 0.55)
+
+            for _ in range(10000):
+
+                self.assertEqual(size, h.size())
+
+                # Do an insert operation
+                if random.random() < bias:
+                    h.insert(random.randint(-1000000, 1000000))
+                    size += 1
+                    self.check_min(h)
+                # Make sure it behaves properly when empty
+                elif size == 0:
+                    self.assertTrue(h.is_empty())
+                    with self.assertRaises(HeapException):
+                        h.find_min()
+                    with self.assertRaises(HeapException):
+                        h.del_min()
+                # Do find and del operations
+                else:
+                    self.assertEqual(h.find_min(), h.del_min())
+                    size -= 1
+                    if size > 0:
+                        self.check_min(h)
 
     def check_min(self, heap):
         self.assertEqual(min(heap.heap[1:]), heap.heap[1], f"\n{heap.heap}")
