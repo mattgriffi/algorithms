c8580a1d72887b5498e5e5e00f55dd3be1b09219
mseffner <mseffner@gmail.com>
2017-06-03 10:28:55 -0700

    implemented constructor for Node class

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -16,3 +16,12 @@ class BinarySearchTree:
 
     def __iter__(self):
         return self.root.__iter__()
+
+
+class Node:
+    def __init__(self, key, value, left=None, right=None, parent=None):
+        self.key = key
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
