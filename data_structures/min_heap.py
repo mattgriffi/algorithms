c3b1401d83bb6ca8002fb3199cf1673d9cd3919f
mseffner <mseffner@gmail.com>
2017-06-02 19:53:23 -0700

    added binary heap stub

---
+++ data_structures/min_heap.py
@@ -0,0 +1,24 @@
+"""This class implements a min binary heap."""
+
+
+class MinHeap:
+    def __init__(self):
+        pass
+
+    def insert(self, k):
+        """Adds new item k into the heap."""
+
+    def find_min(self):
+        """Returns the item with the minimum key value."""
+
+    def del_min(self):
+        """Returns the item with the minimum key value and removes it from the heap."""
+
+    def is_empty(self):
+        """Returns True if the heap is empty, else False."""
+
+    def size(self):
+        """Returns the number of items currently in the heap."""
+
+    def build_heap(self, key_list: list):
+        """Builds a new heap from the keys in the key_list."""
