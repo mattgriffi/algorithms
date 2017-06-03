940f6325aeadbb40150734ec4db82053e1f04d9d
mseffner <mseffner@gmail.com>
2017-06-03 12:07:47 -0700

    added bst unit tester stub

---
+++ tests/bst_tester.py
@@ -0,0 +1,17 @@
+"""Unit tests for binary search tree."""
+
+
+import random
+import unittest
+
+from data_structures.binary_search_tree import BinarySearchTree
+
+
+class BinarySearchTreeTester(unittest.TestCase):
+
+    def setUp(self):
+        self.bst = BinarySearchTree()
+
+
+if __name__ == "__main__":
+    unittest.main()
