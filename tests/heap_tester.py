e78728064c3df4c5d60f32fed36c59c86300c72b
mseffner <mseffner@gmail.com>
2017-06-02 21:54:32 -0700

    added tester for del_min

--- tests/heap_tester.py
+++ tests/heap_tester.py
@@ -16,8 +16,21 @@ class HeapTester(unittest.TestCase):
             self.h.insert(random.randint(-100, 100))
             self.check_min(self.h)
 
+    def test_del_min(self):
+
+        for _ in range(100):
+            self.h.insert(random.randint(-100, 100))
+
+        sorted_list = []
+        for _ in range(99):
+            min_item = self.h.del_min()
+            self.check_min(self.h)
+            sorted_list.append(min_item)
+        self.assertTrue(sorted(sorted_list) == sorted_list)
+
     def check_min(self, heap):
         self.assertEqual(min(heap.heap[1:]), heap.heap[1], f"\n{heap.heap}")
+        self.assertEqual(min(heap.heap[1:]), heap.find_min(), f"\n{heap.heap}")
 
 
 if __name__ == "__main__":
