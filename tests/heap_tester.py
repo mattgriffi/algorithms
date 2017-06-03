37ab2696392c62359f52ce53c50db143f7580213
mseffner <mseffner@gmail.com>
2017-06-02 19:54:39 -0700

    added heap unit test stub

---
+++ tests/heap_tester.py
@@ -0,0 +1,15 @@
+"""Unit tests for binary heaps."""
+
+
+import unittest
+
+from data_structures.min_heap import MinHeap
+
+
+class HeapTester(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+if __name__ == "__main__":
+    unittest.main()
