249331b60fc7657891497de3daacf1d0dcdc77e2
mseffner <mseffner@gmail.com>
2017-06-02 21:31:02 -0700

    implemented __percolate_down, __min_child and fixed size tracking

--- data_structures/min_heap.py
+++ data_structures/min_heap.py
@@ -4,10 +4,12 @@
 class MinHeap:
     def __init__(self):
         self.heap = [0]
+        self.current_size = 0
 
     def insert(self, k):
         """Adds new item k into the heap."""
         self.heap.append(k)
+        self.current_size += 1
         self.__percolate_up()
 
     def find_min(self):
@@ -22,21 +24,24 @@ class MinHeap:
             raise HeapException("cannot del from an empty heap")
         min_item = self.heap[1]
         self.heap[1] = self.heap.pop()
+        self.current_size -= 1
         self.__percolate_down()
         return min_item
 
     def is_empty(self):
         """Returns True if the heap is empty, else False."""
+        return self.current_size == 0
 
     def size(self):
         """Returns the number of items currently in the heap."""
+        return self.current_size
 
     def build_heap(self, key_list: list):
         """Builds a new heap from the keys in the key_list."""
 
     def __percolate_up(self):
         """Takes the last item in the list and moves it up if needed."""
-        c = p = len(self.heap) - 1
+        c = p = self.current_size
         while p > 1:
             p //= 2
             if self.heap[c] < self.heap[p]:
@@ -46,7 +51,21 @@ class MinHeap:
                 break
 
     def __percolate_down(self):
-        pass
+        """Takes the first item in the list and moves down if needed."""
+        p = 1
+        while p * 2 <= self.current_size:
+            min_child = self.__min_child(p)
+            if self.heap[p] < self.heap[min_child]:
+                self.__swap(p, min_child)
+            p = min_child
+
+    def __min_child(self, p):
+        if p * 2 + 1 > self.current_size:
+            return p * 2
+        elif self.heap[p * 2] < self.heap[p * 2 + 1]:
+            return p * 2
+        else:
+            return p * 2 + 1
 
     def __swap(self, x, y):
         self.heap[x], self.heap[y] = self.heap[y], self.heap[x]
