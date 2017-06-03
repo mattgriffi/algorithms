34ec1b12a9c3f14295f199e506e1b86cfa7bfe86
mseffner <mseffner@gmail.com>
2017-06-03 10:36:45 -0700

    implemented basic helper methods in Node

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -25,3 +25,24 @@ class Node:
         self.left = left
         self.right = right
         self.parent = parent
+
+    def is_left_child(self):
+        """Returns True if node is the left child of its parent, else False."""
+        return self.parent and self.parent.left == self
+
+    def is_right_child(self):
+        """Returns True if node is the right child of its parent, else False."""
+        return self.parent and self.parent.right == self
+
+    def is_root(self):
+        """Returns True if node is the root, else False."""
+        return self.parent is None
+
+    def is_leaf(self):
+        """Returns True if node has no children, else False."""
+        return self.left is None and self.right is None
+
+    def has_two_children(self):
+        """Returns True if node has both a left and right child, else false."""
+        return self.left is not None and self.right is not None
+
