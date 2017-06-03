b0ea2fdf3366e9acac992c6bd3430f8362584f4b
mseffner <mseffner@gmail.com>
2017-06-03 11:50:18 -0700

    implemented put

--- data_structures/binary_search_tree.py
+++ data_structures/binary_search_tree.py
@@ -11,6 +11,36 @@ class BinarySearchTree:
         """Returns the number of nodes in the tree."""
         return self.size
 
+    def put(self, key, value):
+        """Adds the key-value pair to the tree."""
+        if self.root is None:
+            self.root = Node(key, value)
+        else:
+            self._put(key, value, self.root)
+
+    def _put(self, key, value, current_node):
+        """Recursively puts the key-value pair in the right place."""
+        # If the key is already in the tree, change its value
+        if key == current_node.key:
+            current_node.value = value
+        # Else if the key is less than the current node
+        elif key < current_node.key:
+            # If current node has a left child, recursively move to that child
+            if current_node.has_left_child():
+                self._put(key, value, current_node.left)
+            # If current node has no left child, create node
+            else:
+                current_node.left = Node(key, value, parent=current_node)
+        # Else if key is greater than the current node
+        else:
+            # If current node has a right child, recursively move to that child
+            if current_node.has_right_child():
+                self._put(key, value, current_node)
+            # If current node has no right child, create node
+            else:
+                current_node.right = Node(key, value, parent=current_node)
+
+
     def __len__(self):
         return self.size
 
