5f8f7b0a9259c145de74062b9d49d48972c4bdf4
mseffner <mseffner@gmail.com>
2017-06-03 21:30:37 -0700

    added AVLTree stub

---
+++ data_structures/avl_tree.py
@@ -0,0 +1,9 @@
+"""This class implements an AVL tree, which is a subclass of BinarySearchTree."""
+
+
+from data_structures.binary_search_tree import BinarySearchTree, Node
+
+
+class AVLTree(BinarySearchTree):
+    def __init__(self):
+        super().__init__()
