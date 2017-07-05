87cac5fe4b3f661179370cdeb27e46a578c7f2bc
mseffner <mseffner@gmail.com>
2017-07-05 11:17:49 -0700

    solved levenshtein distance with DP

---
+++ levenshtein_distance.py
@@ -0,0 +1,31 @@
+import time
+
+
+def main():
+    string_A = 'bb'
+    string_B = 'bb'
+
+    def R(idx_a, idx_b, cache={}):
+        if (idx_a, idx_b) in cache:
+            return cache[(idx_a, idx_b)]
+
+        while idx_a > -1 and idx_b > -1 and string_A[idx_a] == string_B[idx_b]:
+            idx_a, idx_b = idx_a - 1, idx_b - 1
+
+        if idx_a < 0:
+            cache[(idx_a, idx_b)] = idx_b + 1
+        elif idx_b < 0:
+            cache[(idx_a, idx_b)] = idx_a + 1
+        else:
+            cache[(idx_a, idx_b)] = 1 + \
+                min(R(idx_a, idx_b - 1), R(idx_a - 1, idx_b - 1), R(idx_a - 1, idx_b))
+
+        return cache[(idx_a, idx_b)]
+
+    start = time.time()
+    ans = R(len(string_A) - 1, len(string_B) - 1)
+    print(f'Time taken: {time.time()-start}\nAnswer: {ans}')
+
+
+if __name__ == "__main__":
+    main()
